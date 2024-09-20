b1d4431cb8489d78df78db2821fc10445da86ea7
Ryota.Nakahama <ryota.nakahama@access-company.com>
2024-09-20 10:07:01 +0900

    Add license info.

--- Main.cpp
+++ Main.cpp
@@ -91,6 +91,13 @@ void Main()
 				font(U"Failure!!").drawAt(400, 300, Palette::Black);
 			}
 		}
-
+		
+		// "Licenses" ボタンが押されたら
+		if (SimpleGUI::Button(U"Licenses", Vec2{ 20, 20 }))
+		{
+			// ライセンス情報を表示
+			LicenseManager::ShowInBrowser();
+		}
+		
 	}
 }
